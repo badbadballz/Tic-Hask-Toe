321a7580f8f7edca6a4842476e9033ff622fbae2
badbadballz <roy.chan22@gmail.com>
2023-03-29 01:23:55 +1100

    lecture 2 exercises

--- solutions/Sandbox.hs
+++ solutions/Sandbox.hs
@@ -1,1 +1,20 @@
 module Sandbox where
+--import Data.Bits (Bits(xor))
+--import Control.Arrow (Arrow(first))
+
+type Name = String
+type Id = Int
+type Power = String
+
+data Pokemon = MkPokemon Name Id [Power] deriving Show
+
+pikachu = MkPokemon "Pikachu" 20 ["Electric"]
+bulbasaur = MkPokemon "Bulbasaur" 25 ["Grass", "Poison"]
+
+getFirstPower :: Pokemon -> Power
+getFirstPower (MkPokemon _ _ []) = [] 
+getFirstPower (MkPokemon _ _ (x:xs)) = x
+
+fifth :: [a] -> a
+fifth xs = if length xs < 5 then head xs else go xs 
+                where go (_ : (_ : (_ : (_ : (x : _))))) = x
