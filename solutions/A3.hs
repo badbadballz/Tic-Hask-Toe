08d0ac4882cfc95981c6253da9954eb8d28ad78e
badbadballz <roy.chan22@gmail.com>
2023-04-05 00:13:10 +0900

    Solutions for A3, version 1

--- solutions/A3.hs
+++ solutions/A3.hs
@@ -4,6 +4,7 @@ import A1
 import A2
 
 import Data.List (transpose)
+--import System.Posix.Internals (puts)
 --import Data.Monoid (All(getAll))
 
 -- *** Assignment 3-1 ***
@@ -11,7 +12,11 @@ import Data.List (transpose)
 -- Q#01
 showInts :: [Int] -> [String]
 showInts [] = []
-showInts xs = map show xs
+showInts (x:xs) = show x : showInts xs
+
+showInts' :: [Int] -> [String]
+showInts' [] = []
+showInts' xs = map show xs
 
 
 _HEADER_ = formatLine $ showInts _RANGE_
@@ -25,7 +30,11 @@ showSquares (x:xs) = show x : showSquares xs
 
 -- Q#03
 formatRows :: Board -> [String]
-formatRows b = let b' = map showSquares b
+formatRows [] = []
+formatRows (b:bs) = formatLine (showSquares b) : formatRows bs
+
+formatRows' :: Board -> [String]
+formatRows' b = let b' = map showSquares b
                 in map formatLine b'
 
 -- Q#04
@@ -59,7 +68,9 @@ getDiag2 :: Board -> Line
 getDiag2 [[]] = []
 getDiag2 b = go b (_SIZE_ - 1)
                 where go [] _ = []
-                      go (b':bs') i = b' !! i : go bs' (i - 1)
+                      go (b':bs') i 
+                            | i >= 0 = b' !! i : go bs' (i - 1)
+                            | otherwise = [] -- preventing negative index
 
 
 getAllLines :: Board -> Board
@@ -73,16 +84,51 @@ getAllLines b = let t = transpose b
 
 -- Q#07
 
-putSquare = undefined
+putSquare :: Player -> Board -> Move -> Board
+putSquare _ [] _ = []
+putSquare p b m
+        | isMoveInBounds m = go p b m 0 
+        | otherwise = b
+            where go _ [] _ _ = []
+                  go p' (b:bs) (r, c) i 
+                        | r == i = let updatedRow = replaceSquareInRow p' c b
+                                    in updatedRow : bs
+                        | otherwise = b : go p' bs m (i + 1)
+     
 
 -- Q#08
 
-prependRowIndices = undefined
+prependRowIndices :: [String] -> [String]
+prependRowIndices s = go $ indexRowStrings s
+                         where go [] = []
+                               go ((i, x) : xs) = let prepended = i:x 
+                                                    in prepended : go xs   
 
+test1 = [". Learn Haskell", ". Wait for more industry adoption", ". Profit?"]
 -- Q#09
 
-isWinningLine = undefined
+isWinningLine :: Player -> Line -> Bool
+isWinningLine _ [] = False
+isWinningLine p l = go l False
+                     where  go [] b = b 
+                            go (x : xs) b = if x == p then go xs True
+                                            else False
 
+test2 = [X, X ,X]
+test3 = [O, O ,O, O]
+test4 = [X, X, O]
+test5 = [X, O, O]
 -- Q#10
 
-isValidMove = undefined
+isValidMove :: Board -> Move -> Bool
+isValidMove [] _  = False
+isValidMove b m@(r, c) 
+    | isMoveInBounds m = let row = findRow b 0
+                            in isColEmpty row c                           
+    | otherwise = False 
+        where findRow [] _ = []
+              findRow (b':bs') i =  if r == i then b' else findRow bs' (i + 1)
+
+-- can use !! instead??
+--test6 = [X, Empty, O]
+--test7 = [Empty, Empty, O]
