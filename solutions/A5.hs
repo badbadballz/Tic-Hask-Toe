38a203efea64b83a7cd79a9ba27ec96df1a6970d
badbadballz <roy.chan22@gmail.com>
2023-04-14 16:41:12 +0800

    used >> and >>= as 5-1 solutions

--- solutions/A5.hs
+++ solutions/A5.hs
@@ -7,9 +7,6 @@ import A4
 
 import System.Random.Stateful (globalStdGen, uniformM)
 import Control.Monad (when)
---import GHC.Base (magicDict)
---import Data.Monoid (First(getFirst))
---import GHC.Num (bitInteger)
 
 -- *** Assignment 5-1 *** --
 
@@ -24,32 +21,56 @@ _LOGO_PATH_ = "./assets/logo.txt"
 
 
 printLogo :: IO()
-printLogo = do
+printLogo = readFile _LOGO_PATH_ >>= putStrLn 
+
+{- printLogo = do
              f <- readFile _LOGO_PATH_
              putStrLn f
-
+-}
 -- Q#03
 _RANDOM_BOOL_ :: IO Bool
 _RANDOM_BOOL_ = uniformM globalStdGen
 
 firstPlayer :: IO Player
-firstPlayer = do
+firstPlayer = _RANDOM_BOOL_ >>= return.getFirstPlayer
+{- firstPlayer = do
                 result <- _RANDOM_BOOL_
                 return (getFirstPlayer result)
+-}
 -- Q#04
 
 getMove :: Board -> IO Move
-getMove b = do 
+getMove b = getLine >>=
+            (\m -> if isValidMove b (stringToMove m) then
+                      return (stringToMove m)
+                      else 
+                        putStrLn "Invalid move! Try again" >>
+                        getMove b)
+{- getMove b = do 
               m <- getLine
               if isValidMove b (stringToMove m) then
                  return (stringToMove m)
               else do   
                     putStrLn "Invalid move! Try again"
                     getMove b
+-}
 -- Q#05
 
 play :: Board -> Player -> IO()
-play b p = do
+play b p = when _DISPLAY_LOGO_ printLogo >>
+           go b p
+           where 
+            go b' p' = printBoard b' >>
+                       putStrLn (promptPlayer p') >>
+                       getMove b' >>=
+                       (\m -> 
+                        let (gs, b'') = playMove p' b' m
+                           in case gs of
+                                 InProgress -> go b'' (switchPlayer p')
+                                 _          -> printBoard b'' >> putStrLn (showGameState gs)
+                        )
+                       
+{- play b p = do
             when _DISPLAY_LOGO_ printLogo
             go b p
             where go b' p' = do
@@ -62,22 +83,51 @@ play b p = do
                                             _          -> do
                                                             printBoard b''
                                                             putStrLn (showGameState gs)
-
+-}
 
 -- *** Assignment 5-2 *** --
 
 -- Q#07
 
-printLogoDo = undefined
+printLogoDo :: IO()
+printLogoDo = do
+                 f <- readFile _LOGO_PATH_
+                 putStrLn f
+--printLogoDo = undefined
 
 -- Q#08
+firstPlayerDo :: IO Player
+firstPlayerDo = do
+                  result <- _RANDOM_BOOL_
+                  return (getFirstPlayer result)
 
-firstPlayerDo = undefined
+--firstPlayerDo = undefined
 
 -- Q#09
+getMoveDo :: Board -> IO Move
+getMoveDo b = do 
+              m <- getLine
+              if isValidMove b (stringToMove m) then
+                 return (stringToMove m)
+              else do   
+                    putStrLn "Invalid move! Try again"
+                    getMove b
 
-getMoveDo = undefined
+--getMoveDo = undefined
 
 -- Q#10
-
-playDo = undefined
+playDo :: Board -> Player -> IO()
+playDo b p = do
+             when _DISPLAY_LOGO_ printLogo
+             go b p
+             where go b' p' = do
+                                printBoard b'
+                                putStrLn (promptPlayer p')
+                                m <- getMove b'
+                                let (gs, b'') = playMove p' b' m
+                                        in case gs of 
+                                            InProgress -> go b'' (switchPlayer p')
+                                            _          -> do
+                                                            printBoard b''
+                                                            putStrLn (showGameState gs)
+--playDo = undefined
